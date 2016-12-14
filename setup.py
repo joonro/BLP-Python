854a67f13e29045cb0e851a3acaead84f7a9ea3b
Joon Ro <joon.ro@outlook.com>
2016-12-14 10:40:18 -0800

    Modernize cython build

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
+import os
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
 import numpy
 
-setup(cmdclass={'build_ext': build_ext},
-      ext_modules=[Extension("_BLP", ["_BLP.pyx"], 
-      include_dirs=[numpy.get_include()])]
-      )
+if os.name == 'nt':
+    ext_modules = [
+        Extension(
+            "_BLP",
+            ["_BLP.pyx"],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=['/openmp'],
+        )
+    ]
+else:
+    ext_modules = [
+        Extension(
+            "_BLP",
+            ["_BLP.pyx"],
+            libraries=["m"],
+            extra_compile_args=['-fopenmp'],
+            extra_link_args=['-fopenmp'],
+        )
+    ]
+
+setup(
+    name='pyBLP',
+    ext_modules=cythonize(ext_modules),
+)
